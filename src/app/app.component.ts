18f458df3ed44268bf2b693d56dc3c53e59f2a80
mpro7 <justinstarlight@gmail.com>
2017-10-04 23:02:16 +0200

    details are displayed for selected hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,12 @@ export class Hero {
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
 }
 
 const HEROES: Hero[] = [
