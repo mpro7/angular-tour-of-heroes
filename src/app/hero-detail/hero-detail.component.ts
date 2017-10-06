150f29adf4d6341f2c90f23f9b1c860c75f78871
mpro7 <justinstarlight@gmail.com>
2017-10-06 16:16:05 +0200

    added get back method

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -25,5 +25,9 @@ export class HeroDetailComponent implements OnInit {
     .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id'))).subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
   @Input() hero: Hero;
 }
