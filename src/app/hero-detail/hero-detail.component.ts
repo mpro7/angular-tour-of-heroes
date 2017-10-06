afbf04792e24a88cc847362fb94e091b6029c4aa
mpro7 <justinstarlight@gmail.com>
2017-10-06 16:11:15 +0200

    HeroDetailComponent revised

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../services/hero.service';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,9 +14,15 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() {}
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.paramMap
+    .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id'))).subscribe(hero => this.hero = hero);
   }
 
   @Input() hero: Hero;
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
-@Injectable()
 export class HeroService {
 
   constructor() { }
@@ -16,6 +16,10 @@ export class HeroService {
       // Simulate server latency with 2 second delay
       setTimeout(() => resolve(this.getHeroes()), 2000);
     });
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
   }
 
 }
